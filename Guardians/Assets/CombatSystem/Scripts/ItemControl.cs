d4efdeb6666ed928b87e4cdb640717fe9ca52f53
YoonGeunYong <rmsdyd1926@naver.com>
2023-12-11 12:28:34 +0900

    12.11

--- Guardians/Assets/CombatSystem/Scripts/ItemControl.cs
+++ Guardians/Assets/CombatSystem/Scripts/ItemControl.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using BehaviorDesigner.Runtime.Tasks.Unity.UnityVector2;
 using UnityEngine;
 
-public class PursuitEnemy : MonoBehaviour
+public class ItemControl : MonoBehaviour
 {
     private Rabbit parent;
+    private GameObject effect;
     private float speed = 7f;
     private Vector3 dir;
     
@@ -14,6 +15,14 @@ public class PursuitEnemy : MonoBehaviour
         parent = gameObject.GetComponentInParent<Rabbit>();
         if (parent.statsSO._childItemImage is not null)
             GetComponent<SpriteRenderer>().sprite = parent.statsSO._childItemImage;
+        else
+        {
+            foreach(var unitUI in MiniMap.instance.GetMiniMapTile().unitsOnTile)
+            {
+                unitUI.unit.stats.healthPoint += GameController.instance.preStats[(int)UnitTypes.Fairy]._stats.attack;
+            }
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -22,7 +31,7 @@ public class PursuitEnemy : MonoBehaviour
         dir = parent.enemy.gameObject.transform.position - transform.position;
         transform.Translate(dir.normalized * (speed * Time.deltaTime));
         
-        if (Vector3.Distance(parent.enemy.gameObject.transform.position,transform.position) < 1f)
+        if (Vector3.Distance(parent.enemy.gameObject.transform.position, transform.position) < 1f) 
         {
             Destroy(gameObject);
         }
