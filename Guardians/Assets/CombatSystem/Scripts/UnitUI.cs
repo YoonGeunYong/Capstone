950f2c8ddf036d0b2557e7e0afea0a5d2225ac3a
YoonGeunYong <rmsdyd1926@naver.com>
2023-12-18 15:02:52 +0900

    12.18

--- Guardians/Assets/CombatSystem/Scripts/UnitUI.cs
+++ Guardians/Assets/CombatSystem/Scripts/UnitUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,14 @@ public class UnitUI : MonoBehaviour
 
     public Unit             unit;
     public MiniMapTile      CurrentTile { get; set; }  // 이 유닛이 현재 속한 타일
-    public Vector2Int       boardPosition; 
+    public Vector2Int       boardPosition;
+    public Sprite           enemyIcon;
+
+    private void Start()
+    {
+        if (unit.team == Unit.Team.Enemy)
+            GetComponent<SpriteRenderer>().sprite = enemyIcon;
+    }
 }
 
 
